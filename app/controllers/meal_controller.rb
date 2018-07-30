ae968b9c08ad0f702983a8ff236e76a941f038d1
Jacky (Yuxuan) Jin <jackykimjk@icloud.com>
2018-07-31 00:49:15 +0800

    meal controller and router done

---
+++ app/controllers/meal_controller.rb
@@ -0,0 +1,48 @@
+class MealController < ApplicationController
+  before_action :find_meal, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @meals = Meal.all
+  end
+
+  def show
+  end
+
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.new(meal_params)
+    if @meal.save
+      redirect_to meals_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @meal.update(meal_params)
+      redirect_to @meal
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @meal.destroy
+    redirect_to meals_path
+  end
+
+  private
+  def find_meal
+    @meal = Meal.find(params[:id])
+  end
+
+  def meal_params
+    params.require(:meal).permit(:name, :description, :price, :category)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'meal/index'
+  get 'meal/show'
+  get 'meal/new'
+  get 'meal/create'
+  get 'meal/edit'
+  get 'meal/update'
+  get 'meal/destroy'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :meals
 end
