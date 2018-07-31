b9ba04eae2e0caf01ebd9560bfbe1a5af6047756
Jacky (Yuxuan) Jin <jackykimjk@icloud.com>
2018-07-31 16:13:12 +0800

    user profile done, with minimum layouts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,10 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def profile
+    if user_signed_in?
+      @user = current_user
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :meals
   resources :users
+
+  get "profile", to: "pages#profile", as: "profile"
 end
