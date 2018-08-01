6039c24829f94650b0d0dd599da33dbb9307719f
Jacky (Yuxuan) Jin <jackykimjk@icloud.com>
2018-08-01 17:01:19 +0800

    sign up bug fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :interest, :description])
   end
 end
