d4950206b8b3134beb8852d67e55d4b747add04a
Yikai Qian <yikaiqian@gmail.com>
2018-08-01 17:16:34 +0800

    Added bookings new and meals show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'meals#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :meals
+  resources :meals do
+    get 'bookings/new', to: 'bookings#new'
+  end
   resources :users
 
   get "profile", to: "pages#profile", as: "profile"
